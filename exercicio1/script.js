82a9492ba3d4e69108b73de0283b2e9f34888db0
garrcastro <garrcastro@gmail.com>
2022-11-08 10:42:25 -0300

    Exercício 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,18 @@
+const sacolao =[]
+const fruta1 = {
+    nome: "banana",
+    preco: 4.00,
+    disponibilidade: true
+}
+const fruta2 = {
+    nome: "maçã",
+    preco: 2.00,
+    disponibilidade: false
+}
+const fruta3 ={
+    nome: "umbú",
+    preco: 3.00,
+    disponibilidade: true
+}
+sacolao.push(fruta1,fruta2,fruta3)
+console.log(sacolao)
